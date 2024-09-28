83d7cc6565fdbc4c55bf9c84f8c4a9370cf738ee
brayanmedinacardozo96 <brayanmedinacardozo@gmail.com>
2024-09-28 18:08:18 -0500

    feat:UIlabel dart

--- build/components/labels/index.js
+++ build/components/labels/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 const UILabel = (props) => {
     var _a;
-    return (React.createElement("label", { id: props.id, htmlFor: props.htmlFor, className: (_a = props.className) !== null && _a !== void 0 ? _a : 'text-xs text-gray-900' }, props.text));
+    return (React.createElement("label", { id: props.id, htmlFor: props.htmlFor, className: (_a = props.className) !== null && _a !== void 0 ? _a : 'text-xs' }, props.text));
 };
 export default UILabel;
